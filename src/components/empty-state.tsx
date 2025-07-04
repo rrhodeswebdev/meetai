9596b4330860e0f0486dbf58465bfd2154c4eadc
Ryan Rhodes <ryan@ryanrhodes.io>
2025-07-04 17:12:48 -0400

    completed

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+
+type Props = {
+	title: string;
+	description: string;
+};
+
+export function EmptyState({ title, description }: Props) {
+	return (
+		<div className="flex flex-col items-center justify-center">
+			<Image
+				src="/empty.svg"
+				alt="Empty State Logo"
+				width={240}
+				height={240}
+			/>
+			<div className="mx-auto flex max-w-md flex-col gap-y-6 text-center">
+				<h6 className="text-lg font-medium">{title}</h6>
+				<p className="text-muted-foreground text-sm">{description}</p>
+			</div>
+		</div>
+	);
+}
---
+++ src/modules/agents/ui/components/data-table.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import {
+	ColumnDef,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+
+type DataTableProps<TData, TValue> = {
+	columns: ColumnDef<TData, TValue>[];
+	data: TData[];
+	onRowClick?: (row: TData) => void;
+};
+
+export function DataTable<TData, TValue>({
+	columns,
+	data,
+	onRowClick,
+}: DataTableProps<TData, TValue>) {
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<div className="bg-background overflow-hidden rounded-lg border">
+			<Table>
+				<TableBody>
+					{table.getRowModel().rows?.length ? (
+						table.getRowModel().rows.map(row => (
+							<TableRow
+								key={row.id}
+								data-state={row.getIsSelected() && "selected"}
+								className="cursor-pointer"
+								onClick={() => onRowClick?.(row.original)}
+							>
+								{row.getVisibleCells().map(cell => (
+									<TableCell
+										key={cell.id}
+										className="p-4 text-sm"
+									>
+										{flexRender(
+											cell.column.columnDef.cell,
+											cell.getContext()
+										)}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell
+								colSpan={columns.length}
+								className="text-muted-foreground h-19 text-center"
+							>
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+		</div>
+	);
+}
