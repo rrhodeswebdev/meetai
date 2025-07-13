5fba3c88d86d34a318ab0f764a4da23f78d8b601
Ryan Rhodes <ryan@ryanrhodes.io>
2025-07-12 22:04:34 -0400

    completed (#18)

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,4 +1,5 @@
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/meetings/params";
 import { MeetingsListHeader } from "@/modules/meetings/ui/components/meetings-list-header";
 import {
 	MeetingsView,
@@ -9,10 +10,16 @@ import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
-export default async function MeetingsPage() {
+type Props = {
+	searchParams: Promise<SearchParams>;
+};
+
+export default async function MeetingsPage({ searchParams }: Props) {
+	const filters = await loadSearchParams(searchParams);
 	const session = await auth.api.getSession({
 		headers: await headers(),
 	});
@@ -22,7 +29,9 @@ export default async function MeetingsPage() {
 	}
 
 	const queryClient = getQueryClient();
-	void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+	void queryClient.prefetchQuery(
+		trpc.meetings.getMany.queryOptions({ ...filters })
+	);
 
 	return (
 		<>
--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -11,6 +11,7 @@ import { TRPCError } from "@trpc/server";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { z } from "zod";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
+import { MeetingStatus } from "../types";
 
 export const meetingsRouter = createTRPCRouter({
 	getMany: protectedProcedure
@@ -23,10 +24,20 @@ export const meetingsRouter = createTRPCRouter({
 					.max(MAX_PAGE_SIZE)
 					.default(DEFAULT_PAGE_SIZE),
 				search: z.string().nullish(),
+				agentId: z.string().nullish(),
+				status: z
+					.enum([
+						MeetingStatus.Upcoming,
+						MeetingStatus.Active,
+						MeetingStatus.Completed,
+						MeetingStatus.Processing,
+						MeetingStatus.Cancelled,
+					])
+					.nullish(),
 			})
 		)
 		.query(async ({ ctx, input }) => {
-			const { page, pageSize, search } = input;
+			const { page, pageSize, search, status, agentId } = input;
 
 			const data = await db
 				.select({
@@ -42,7 +53,11 @@ export const meetingsRouter = createTRPCRouter({
 				.where(
 					and(
 						eq(meetings.userId, ctx.auth.user.id),
-						search ? ilike(meetings.name, `%${search}%`) : undefined
+						search
+							? ilike(meetings.name, `%${search}%`)
+							: undefined,
+						status ? eq(meetings.status, status) : undefined,
+						agentId ? eq(meetings.agentId, agentId) : undefined
 					)
 				)
 				.orderBy(desc(meetings.createdAt), desc(meetings.id))
@@ -56,7 +71,11 @@ export const meetingsRouter = createTRPCRouter({
 				.where(
 					and(
 						eq(meetings.userId, ctx.auth.user.id),
-						search ? ilike(meetings.name, `%${search}%`) : undefined
+						search
+							? ilike(meetings.name, `%${search}%`)
+							: undefined,
+						status ? eq(meetings.status, status) : undefined,
+						agentId ? eq(meetings.agentId, agentId) : undefined
 					)
 				);
 
